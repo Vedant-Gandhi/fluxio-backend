15551cbebbab6e5c5765ebd5eeb4ef118aec9389
Vedant-Gandhi <vedantgandhipersonal@gmail.com>
2025-04-22 01:28:25 +0530

    feat: add user table.

---
+++ pkg/repository/pgsql/init.go
@@ -0,0 +1,33 @@
+package pgsql
+
+import (
+	"fluxio-backend/pkg/repository/pgsql/tables"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type PgSQLConfig struct {
+	url string
+}
+
+type PgSQL struct {
+	db *gorm.DB
+}
+
+func NewPgSQL(cfg PgSQLConfig) (*PgSQL, error) {
+
+	db, err := gorm.Open(postgres.Open(cfg.url), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Automatically migrate the schema, keeping your database up to date.
+	db.AutoMigrate(&tables.User{})
+
+	return &PgSQL{
+		db: db,
+	}, nil
+
+}
