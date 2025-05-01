7cdba2066017123f0c1846bbe16f07c63dc81b7c
Vedant Gandhi <vedantgandhipersonal@gmail.com>
2025-05-01 23:59:31 +0530

    feat: create video router.

---
+++ pkg/transport/http/routes/video.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"fluxio-backend/pkg/transport/http/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+type VideoRouter struct {
+	VideoController *controller.VideoController
+}
+
+func NewVideoRouter(VideoController *controller.VideoController) *VideoRouter {
+	return &VideoRouter{
+		VideoController: VideoController,
+	}
+}
+
+// RegisterRoutes registers all Video-related routes
+func (r *VideoRouter) RegisterRoutes(router *gin.Engine) {
+	VideoGroup := router.Group("/api/v1/Video")
+	{
+		VideoGroup.POST("/upload", r.VideoController.GenerateVideUploadEntry)
+
+	}
+}
