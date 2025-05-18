8a1d305da9b3252a7d34f6bd8baf976f7a6e8b29
Vedant-Gandhi <vedantgandhipersonal@gmail.com>
2025-05-18 14:09:05 +0530

    feat: add a logger.

---
+++ pkg/logger/logger.go
@@ -0,0 +1,44 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/rs/zerolog/log"
+)
+
+type DefaultLogger struct{}
+
+func NewCmdLogger() *DefaultLogger {
+	return &DefaultLogger{}
+}
+
+func (c *DefaultLogger) Info(msg string, data ...interface{}) {
+	if len(data) > 0 {
+		log.Info().Msgf("%s: %v", msg, data)
+		return
+	}
+
+	log.Info().Msg(msg)
+}
+
+func (c *DefaultLogger) Warn(msg string) {
+	log.Warn().Msg(msg)
+}
+
+func (c *DefaultLogger) Debug(msg string, data ...interface{}) {
+	isDevelop := os.Getenv("GO_ENV") == "development"
+
+	if !isDevelop {
+		return
+	}
+	if len(data) > 0 {
+		log.Debug().Msgf("%s: %v", msg, data)
+		return
+	}
+
+	log.Debug().Msg(msg)
+}
+
+func (c *DefaultLogger) Error(msg string, err error) {
+	log.Error().Err(err).Msg(msg)
+}
